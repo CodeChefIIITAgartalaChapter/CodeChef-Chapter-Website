8eec9866b3185da6a4fc3031688279c3891f4643
CodeChef IIIT Agartala Chapter <96460526+CodeChefIIITAgartalaChapter@users.noreply.github.com>
2021-12-21 23:03:34 +0530

    Update settings.py

--- ChapterWebsite/settings.py
+++ ChapterWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chapteriiitagt.herokuapp.com','cciiitagtchapter.live']
+ALLOWED_HOSTS = ['chapteriiitagt.herokuapp.com','cciiitagtchapter.live','www.cciiitagtchapter.live']
 
 
 # Application definition
