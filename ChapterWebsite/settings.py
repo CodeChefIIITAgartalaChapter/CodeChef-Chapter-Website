57213d277f2eb9874e79a46d534a8fe6ff127429
CodeChef IIIT Agartala Chapter <96460526+CodeChefIIITAgartalaChapter@users.noreply.github.com>
2021-12-21 15:06:39 +0530

    Update settings.py

--- ChapterWebsite/settings.py
+++ ChapterWebsite/settings.py
@@ -16,6 +16,7 @@ import os
 
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -117,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
